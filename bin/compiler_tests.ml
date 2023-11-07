e85e7904a65edca0a3289b4c74be3cd1045a49e7
fresheed <ok.john.rus@gmail.com>
2023-11-07 13:51:37 +0100

    Slight refactoring

--- bin/compiler_tests.ml
+++ bin/compiler_tests.ml
@@ -2,6 +2,9 @@ open Mylex
 open Exercises
 open Ex2_defs_ext
 
+(** A dummy "typechecking" stage that only checks the absence of explicit zero divisions.
+    Also note that in this simple case the untyped AST and typed AST have the same OCaml type.    
+*)
 let rec check_div0 = function
 | BinOp (Div, _, Int 0) -> ["Division by zero found"]
 | BinOp (op, e1, e2) -> check_div0 e1 @ check_div0 e2
@@ -11,33 +14,47 @@ let check_div0_s (s: estmt) = match s with
 | Val (_, e) -> check_div0 e
 | Input _ -> []
 
-let typecheck ((ess, ex): eprog) = 
+let typecheck ((ess, ex): eprog): eprog * string list = 
   let errs = List.concat (List.map check_div0_s ess) @ (check_div0 ex) in
   ((ess, ex), errs)
 
+(***)
+
+(** An even more dummy "compilation" stage that ignores the input 
+    and produces the same shell script *)
+
 let compile (ep: eprog): string =
   "echo 5; echo 7"
+
+(***)
 
 let write_to_file (path: string) (contents: string) =
   let oc = open_out path in
   Printf.fprintf oc "%s" contents;
   close_out oc
 
-let test_compiled sh =
-  let chan = Unix.open_process_in (Printf.sprintf "chmod +x %s; ./%s" sh sh) in
+(** Execute the file at 'path' and return the list of integer it prints *)
+let run_compiled path =
+  (* chmod here allows to run the shell script directly. 
+     You don't need that in actual compilers tests, 
+     since binary produced by clang is already runnable *)
+  let chan = Unix.open_process_in (Printf.sprintf "chmod +x %s; ./%s" path path) in
   let out = In_channel.input_all chan in
-  print_endline out;
   let outs = List.map int_of_string (List.filter (fun s -> s <> "") (String.split_on_char '\n' out)) in
   outs
-  
+
+
 type test_result = LexingError | ParserError | TypeError | Compiled of (int list)
 
+(*** An entry (base, result) means that the result of 'run_compiler {base}.txt'
+     is expected to be exactly 'result' *)
 let expected_results = [
   ("input", Compiled [5; 7]);
   ("input2", TypeError);
 ]
 
-    
+(** Runs all compilation stages, stopping at the first failing one (if any).
+    In your tests you should also check whether lexing/parsing were successful. *)
 let run_compiler file_base: test_result = 
   let file_in = open_in (file_base ^ ".txt") in 
   let lex_buf = Lexing.from_channel file_in in 
@@ -46,11 +63,12 @@ let run_compiler file_base: test_result =
   if (List.length errs > 0) 
   then
     (Printf.printf "Type errors: %s\n" (String.concat ", " errs);
-    TypeError)
+     TypeError)
   else
     let code = compile tast in
     write_to_file (file_base ^ ".sh") code;
-    Compiled (test_compiled (file_base ^ ".sh"))
+    let outs = run_compiled (file_base ^ ".sh") in
+    Compiled outs
 
 let test_file file_base exp: bool * string = 
   let res = run_compiler file_base in
@@ -58,7 +76,12 @@ let test_file file_base exp: bool * string =
   let msg = Printf.sprintf "Output matches expected: %s\n" (if ok then "true" else "false") in
   (ok, msg)
 
-let test = 
+let run_tests () = 
+  (* Here we don't use the messages produced by test_file, 
+     but ideally they should be printed for failing tests *)
   let results = List.map (fun (fb, e) -> test_file fb e) expected_results in
   let res = List.for_all (fun x -> fst x) results in
   Printf.printf "Tests passed: %s\n" (if res then "true" else "false")
+
+
+let _ = run_tests ()
